9db3253746ed3a1d98ee5c846ad39e87280b2b2b
Daisy Good <daisy.m.good@gmail.com>
2016-10-17 15:25:06 -0500

    Add services

---
+++ public/services.js
@@ -0,0 +1,28 @@
+var services = angular.module('Recipe.services', []);
+
+services.factory('Recipes', function ($http) {
+  var retrieveAll = function () {
+    return $http({
+      method:'GET',
+      url:'/api/recipes'
+    });
+    .then(function (resp) {
+      console.log(resp);
+      return resp;
+    })
+  };
+
+  var addMore = function (data) {
+    return $http({
+      method:'POST',
+      url:'/api/recipes',
+      data: data
+    });
+  }
+
+  return {
+    retrieveAll: retrieveAll,
+    addMore: addMore
+  }
+
+})
